8880bb7e3eec714619ff79fc32b4dcf871336cec
heuristic_machina <machine_learner@yahoo.com>
2025-06-16 23:53:08 -0700

    adding columns with mutate(), row subsetting with filter(), column subsetting with select(), where(), and helper functions.

--- exercises/tidyverse.R
+++ exercises/tidyverse.R
@@ -17,3 +17,28 @@ filter(murders, rate <= .71)
 #>3 New Hampshire  NH     Northeast    1316470     5 0.3798036
 #>4  North Dakota  ND North Central     672591     4 0.5947151
 #>5       Vermont  VT     Northeast     625741     2 0.3196211
+
+#>4.2.3 Column-wise subsetting
+#>new object created using dplyr select() streamlining data frame
+#>filter() passes the conditional to the new object
+new_dataframe <- select(murders, state, region, rate)
+filter(new_dataframe, rate <= .71)
+#>         state          region         rate
+#>1        Hawaii         West           0.5145920
+#>2        Iowa           North Central  0.6893484
+#>3        New Hampshire  Northeast      0.3798036
+#>4        North Dakota   North Central  0.5947151
+#>5        Vermont        Northeast      0.3196211
+
+
+#>helper functions
+#>example shows where() helper function selecting all numeric columns
+new_dataframe <- select(murders, where(is.numeric))
+names(new_dataframe)
+#>[1] "population" "total"      "rate" 
+#>
+#>more helper functions: starts_with, ends_with, contains, matches, and num_range
+#>starts_with() example
+new_dataframe <- select(murders, starts_with('r'))
+names(new_dataframe)
+#>[1] "region" "rate"  
