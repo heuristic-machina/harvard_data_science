41847afff48fa6480a2268c9da4165a0965de91c
heuristic_machina <machine_learner@yahoo.com>
2025-06-23 17:32:36 -0700

    adding/transforming variables with := and quotes for multiple

--- 5.1_refining_data_tables.R
+++ 5.1_refining_data_tables.R
@@ -7,7 +7,14 @@ library(data.table)
 murders_dt <- as.data.table(murders)
 
 #5.1.1 column-wise subsetting
-#similar to subsetting matrices
+#similar to subsetting matrices like dplyr's select()
 murders_dt[, c('state', 'region')]
 #or use .() signifying column names instead of string format
 murders_dt[, .(state, region)]
+
+#5.1.2 adding/transforming variables
+#similar to dplyr's mutate() but does not reassign so is optimized for
+#computing resources
+murders_dt[, rate := total / population * 100000]
+#adding multiple columns with quotes
+murders_dt[, ":="(rate = total / population * 100000, rank = rank(population))]
