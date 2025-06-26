bbc2d172ef575cc74bf16820ebd1de9a1f841aca
heuristic_machina <machine_learner@yahoo.com>
2025-06-25 22:45:50 -0700

    skewed population correction with scale_x_log10() and scale_y_log10()

--- data_visualization/exercises/dv8_geometries.R
+++ data_visualization/exercises/dv8_geometries.R
@@ -8,3 +8,7 @@ murders |> ggplot(aes(population, total, label=abb)) + geom_label(color='blue')
 
 #mapping color to label
 murders |> ggplot(aes(population, total, label=abb, color=region)) + geom_label()
+
+#14 change the x and y axes to log10 to account for the skewed population distribution
+p <- murders |> ggplot(aes(population, total, label=abb, color=region)) + geom_label()
+p + scale_x_log10() + scale_y_log10()
