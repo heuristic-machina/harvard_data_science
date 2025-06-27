b9b638d9f95cb989c89c8e60dcbbee4d2c3cbca4
heuristic_machina <machine_learner@yahoo.com>
2025-06-26 23:47:22 -0700

    smallpox image plot

--- data_visualization/exercises_dv7-10/dv10_in_practice.R
+++ data_visualization/exercises_dv7-10/dv10_in_practice.R
@@ -1,49 +1,22 @@
-#10.1 Data visualization in practice
+#Data Visualization 10.11 Exercises
 
-#10.3 faceting
-#facet_grid()
-#comparing two variables year~continent
-filter(gapminder, year %in% c (1970, 2010)) |> 
-  ggplot(aes(infant_mortality, life_expectancy, col= continent)) + 
-  geom_point() + facet_grid(year~continent)
+#1 Reproduce the image plot we previously made but for smallpox. For this plot,
+#do not include years in which cases were not reported in 10 or more weeks.
+library(tidyverse)
+library(RColorBrewer)
+library(dslabs)
+data(us_contagious_diseases)
+names(us_contagious_diseases)
 
-#analysis with one variable
-#facet_grid(.~x)
-filter(gapminder, year %in% c(1962, 2012)) |>
-  ggplot(aes(fertility, life_expectancy, col = continent)) +
-  geom_point() +
-  facet_grid(. ~ year)
-
-#10.3.1 facet_wrap() for viewing time transformation
-gapminder |> filter(year %in% years) |>
-  ggplot(aes(life_expectancy, infant_mortality, col = continent)) +
-  geom_point() +
-  facet_wrap(~year)
-
-#10.4 time series plot
-#continent fertility trends
-#americas
-amerfert <- gapminder |> filter(continent == 'Americas') |> 
-  ggplot(aes(year, fertility)) +
-  geom_point() +
-  ggtitle('Americas Fertility 50 years')
-#europe
-eurofert <- gapminder |> filter(continent == 'Europe') |>
-  ggplot(aes(year, fertility)) +
-  geom_point() +
-  ggtitle('Europe Fertility 50 years')
-#africa
-afrifert <- gapminder |> filter(continent == 'Africa') |>
-  ggplot(aes(year, fertility)) +
-  geom_point() +
-  ggtitle('Africa Fertility 50 years')
-#asia
-asiafert <- gapminder |> filter(continent == 'Asia') |>
-  ggplot(aes(year, fertility)) +
-  geom_point() +
-  ggtitle('Asia Fertility 50 years')
-#oceania
-oceanfert <- gapminder |> filter(continent == 'Oceania') |>
-  ggplot(aes(year, fertility)) +
-  geom_point() +
-  ggtitle('Oceania Fertility 50 years')
+dat <- us_contagious_diseases |>
+  filter(!state%in% c('Hawaii', 'Alaska') 
+         & disease == the_disease & weeks_reporting >= 10) |>
+  mutate(rate=count/population*10000*52/weeks_reporting) |>
+  mutate(state=reorder(state, ifelse(year<=1963, rate, NA), 
+                       median, na.rm=TRUE))
+dat |> ggplot(aes(year, state, fill=rate)) +
+  geom_tile(color='grey50') + scale_x_continuous(expand=c(0,0)) +
+  scale_fill_gradientn(colors = brewer.pal(9, "Reds"), trans='sqrt') +
+  geom_vline(xintercept=1963, col='blue') + theme_minimal() +
+  theme(panel.grid=element_blank(), legend.position='bottom', 
+        text=element_text(size=8)) + labs(title=the_disease, x='', y='')
