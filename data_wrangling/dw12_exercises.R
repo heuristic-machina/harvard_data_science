1a54f187f211c34152e9490d73d87787d111c137
heuristic_machina <machine_learner@yahoo.com>
2025-07-01 19:57:26 -0700

    modified ex1 data frame and used right_join() with Peoples dataframe creating specific table

--- data_wrangling/dw12_exercises.R
+++ data_wrangling/dw12_exercises.R
@@ -23,3 +23,27 @@ ex1 <- left_join(top, People, by='playerID') |>
 #3  42 daviskh01     Khris       Davis
 #4  42 doziebr01     Brian      Dozier
 #5  42 encared01     Edwin Encarnacion
+
+#3. In a previous exercise, we created a tidy version of the co2 dataset:
+
+co2_wide <- data.frame(matrix(co2, ncol = 12, byrow = TRUE)) |> 
+  setNames(1:12) |>
+  mutate(year = 1959:1997) |>
+  pivot_longer(-year, names_to = "month", values_to = "co2") |>
+  mutate(month = as.numeric(month))
+
+#We want to see if the monthly trend is changing, so we are going to remove 
+#the year effects and then plot the results. We will first compute the year 
+#averages. Use the group_by and summarize to compute the average co2 for each 
+#year. Save in an object called yearly_avg.
+
+yearly_avg <- co2_wide |> group_by(year) |> summarize(yearly_avg = mean(co2))
+# A tibble: 6 × 2
+#year yearly_avg
+#<int>      <dbl>
+#1  1959       316.
+#2  1960       317.
+#3  1961       317.
+#4  1962       318.
+#5  1963       319.
+#6  1964       319.
