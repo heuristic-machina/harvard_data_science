65b7338c7cc5c4ea6a09ba2138f1172ef007f274
heuristic_machina <machine_learner@yahoo.com>
2025-08-15 03:23:09 -0700

    sample(x, replace=TRUE) similar behavior to replicate()

--- probabilites/discrete_prob.R
+++ probabilites/discrete_prob.R
@@ -5,3 +5,34 @@ beads
 
 #pick bead at random
 sample(beads, 1)
+#replicate() repeats task any number of times i.e. 10000
+B <- 10000
+events <- replicate(B, sample(beads, 1))
+
+#verify distribution results using table
+tab <- table(events)
+tab
+#events
+#blue  red 
+#6094 3906 
+
+#proportions table
+prop.table(tab)
+#events
+# blue    red 
+#0.6094 0.3906 
+
+#sample() showing default behavior of replace=False
+sample(beads, 5)
+#> [1] "red"  "blue" "blue" "blue" "red"
+sample(beads, 5)
+#> [1] "red"  "red"  "blue" "blue" "blue"
+sample(beads, 5)
+#> [1] "blue" "red"  "blue" "red"  "blue"
+
+#replace=true behaves like replicate()
+events <- sample(beads, B, replace = TRUE)
+prop.table(table(events))
+#> events
+#blue    red 
+#0.6017 0.3983
