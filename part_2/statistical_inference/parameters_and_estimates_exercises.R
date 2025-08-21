bd001c3f527750ce1790c103cd54ca63ff0f1acd
heuristic_machina <machine_learner@yahoo.com>
2025-08-20 23:38:56 -0700

    using sums of random variables to derive expected value of d then finding its standard error

--- part_2/statistical_inference/parameters_and_estimates_exercises.R
+++ part_2/statistical_inference/parameters_and_estimates_exercises.R
@@ -38,3 +38,35 @@ plot(p, se)
 
 #6. Copy the code above and put it inside a for-loop to make 
 #the plot for N=25, N=100, and N=1000.
+for (N in c(25, 100, 1000)) {
+  p<-seq(0, 1, length=100)
+  SE<-sqrt(p*(1-p)/N)
+  plot(p, SE)
+}
+
+#7. If we are interested in the difference in proportions, p−(1−p), 
+#our estimate is d=X−(1−X). Use the rules we learned about sums 
+#of random variables and scaled random variables to derive the 
+#expected value of d.
+
+#d=X-(1-X)=X-1+X=2X-1
+#ev_p= p-(1-p)= p-1+p=2p-1
+
+#8.What is the standard error of d?
+#se_d=2*sqrt((p*(1-p))/n)
+
+#9. If the actual p=.45, it means the Republicans are
+# winning by a relatively large margin, since d= -.1 , which
+# is a 10% margin of victory. In this case, what is
+# the standard error of 2X-1 if we take a sample of N=25?
+p<-.45
+se<-2*sqrt((p*(1-p))/25)
+se
+#[1] 0.1989975
+
+#10. Given the answer to 9, which of the following best 
+#describes your strategy of using a sample size of N=25?
+
+#b.Our standard error is larger than the difference, so the 
+#chances of 2X-1 representing a large margin are not small.
+#We should pick a larger sample size
