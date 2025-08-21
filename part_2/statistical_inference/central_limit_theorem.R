b843a2037f697c5470fdb36f2c33ffb3e2bcc003
heuristic_machina <machine_learner@yahoo.com>
2025-08-21 03:53:58 -0700

    estimating p with central limit pnorm after deriving standard error

--- part_2/statistical_inference/central_limit_theorem.R
+++ part_2/statistical_inference/central_limit_theorem.R
@@ -12,11 +12,26 @@ se<-sqrt(x_est*(1-x_est)/25)
 se
 #[1] 0.09991997
 
+#pnorm - gives percentage to the left (or less than) of a given point
+#1-pnorm - gives percentage greater than a given point
 #pnorm(q, mean = 0, sd = 1, lower.tail = TRUE, log.p = FALSE)
-#now pnorm is used to find probability within 1% of p now that we have
+
+#below pnorm is used to find probability within 1% of p now that we have
 #the standard error
 
+#use the quantile, q of 1% and se of 0.0999 to find mean probability
 pnorm(0.01/se)-pnorm(-0.01/se)
 #[1] 0.07971926
 
+
+#7.2 the spread
+#mu=p-(1-p)=2p-1
+
+#7.4 exercises
+
+#1.1. Write an urn model function that takes the 
+#proportion of Democrats and the sample size N as 
+#arguments, and returns the sample average if 
+#Democrats are 1s and Republicans are 0s. Call 
+#the function take_sample.
 
