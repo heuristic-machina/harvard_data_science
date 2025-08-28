2135941519bfc091c0d9ec21fbef5bf707df7196
heuristic_machina <machine_learner@yahoo.com>
2025-08-28 03:07:37 -0700

    bootstrap median and monte carlo simulation approximating confidence interval very close to theoretical distribution

---
+++ part_2/statistical_inference/SI_10_bootstrap/SI_10.R
@@ -0,0 +1,55 @@
+#Statistical Inference 10: Bootstrap
+
+#10.1 median income example
+set.seed(1995)
+n <- 10^6
+income <- 10^(rnorm(n, log10(45000), log10(3)))
+hist(income/10^3, nclass = 1000)
+
+m <- median(income)
+m
+#> [1] 44939
+ 
+#> if access to whole application is unavailable a sample
+#> can be used instead
+N<-100
+x<-sample(income, N)
+median(x)
+#[1] 38461.33
+
+#10.2 confidence intervals for the median
+library(gridExtra)
+B<-10^4
+m<-replicate(B, {
+  x<-sample(income, N)
+  median(x)
+})
+hist(m, nclass=30)
+qqnorm(scale(m)); abline(0,1)
+
+#when we don't have the distribution we used the central
+#limit theorem
+median(x) + 1.96*sd(x)/sqrt(N)*c(-1,1)
+#[1] 21017.93 55904.72
+#comparing the above clt given a known distribution confidence
+# interval
+quantile(m, c(.025, .975))
+#2.5%    97.5% 
+#34607.70 58802.04 
+
+#Bootstrap allows resampling of the sample using a monte carlo
+#simulation with replacement then taking the median of the
+#simulation.  The simulation takes the same size as the original
+#dataset
+B<-10^4
+m_star<-replicate(B, {
+  x_star<-sample(x, N, replace=TRUE)
+  median(x_star)
+})
+
+#bootstrap mimics confidence interval below using the simulation
+#median.  The results are very close to the theoretical 
+#distribution.
+quantile(m_star, c(0.025, 0.975))
+#2.5%    97.5% 
+#30252.87 57046.02 
