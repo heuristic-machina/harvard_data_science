e486505d80075f0393b73d7d850215211a716d9b
heuristic_machina <machine_learner@yahoo.com>
2025-08-27 21:43:34 -0700

    hypothesis testing not rejecting null hypothesis given alpha and p-values; monte carlo simulation finding p-values less than 0.05 and 0.01

---
+++ part_2/statistical_inference/SI_9_hypothesis_testing/SI_9.R
@@ -0,0 +1,84 @@
+#Statisitcal Inference 9: Hypothesis Testing
+
+#9.1 p-values
+
+#Phrasing research asking questions, for example:
+#Does a medication extend the lives of cancer patients?
+#Does an increase in gun sales correlate with more gun violence?
+#Does class size affect test scores?
+
+#Exercises 9.3
+
+#1 Generate a sample of size N=1000 from an urn model with 50% 
+#blue beads:
+N <- 1000
+theta <- 0.5
+x <- rbinom(N, 1, 0.5)
+x_hat<-mean(x)
+x_hat
+#[1] 0.478
+se<-sqrt(x_hat*(1-x_hat)/N)
+#[1] 0.01579465
+z <- (x_hat - theta) / se
+#[1] -1.456189
+p_value <- 2 *(1-pnorm(abs(z)))
+p_value
+#[1] 0.1453403
+#Hypothesis H0: p=0.5 vs H1 != 0.5 with an alpha=0.05
+#the p_value > alpha, so the null hypothesis is not rejected
+
+#then, compute a p-value if theta=0.5. Repeat this 10,000 times
+# and report how often the p-value is lower than 0.05?
+N <- 10000
+theta <- 0.5
+x <- rbinom(N, 1, 0.5)
+x_hat<-mean(x)
+x_hat
+#[1] 0.5002
+se<-sqrt(x_hat*(1-x_hat)/N)
+#[1] 0.005
+z <- (x_hat - theta) / se
+#[1] 0.04
+p_value <- 2 *(1-pnorm(abs(z)))
+p_value
+#[1] 0.9680931
+#Hypothesis H0: p=0.5 vs H1 != 0.5 with an alpha=0.05
+#the p_value > alpha, so the null hypothesis is not rejected
+
+#Finding how often less than 0.05
+set.seed(2)
+
+M <- 1000
+N <- 10000
+p0 <- 0.5
+p1 <- 0.505                       # try 0.505, 0.51, 0.52, etc.
+
+pvals <- replicate(M, {
+  k <- rbinom(1, N, p1)
+  phat <- k / N
+  se <- sqrt(phat * (1 - phat) / N)
+  z <- (phat - p0) / se
+  2 * (1 - pnorm(abs(z)))
+})
+
+mean(pvals < 0.05)                 # estimated power at p1
+#[1] 0.168
+
+#How often is it lower than 0.01?
+set.seed(2)
+
+M <- 1000
+N <- 10000
+p0 <- 0.5
+p1 <- 0.505                       # try 0.505, 0.51, 0.52, etc.
+
+pvals <- replicate(M, {
+  k <- rbinom(1, N, p1)
+  phat <- k / N
+  se <- sqrt(phat * (1 - phat) / N)
+  z <- (phat - p0) / se
+  2 * (1 - pnorm(abs(z)))
+})
+
+mean(pvals < 0.01)                 # estimated power at p1
+#[1] 0.054
