1a3e45bb18517983159e23edf4c54253abf0ca34
heuristic_machina <machine_learner@yahoo.com>
2025-08-28 18:41:46 -0700

    monte carlo simulation for expected value and standard error in the 75th quantile

--- part_2/statistical_inference/SI_10_bootstrap/SI_10.R
+++ part_2/statistical_inference/SI_10_bootstrap/SI_10.R
@@ -53,3 +53,37 @@ m_star<-replicate(B, {
 quantile(m_star, c(0.025, 0.975))
 #2.5%    97.5% 
 #30252.87 57046.02 
+
+#10.3 exercises
+#1. Generate a random dataset like this:
+y<-rnorm(100, 0, 1)
+median(y)
+#[1] -0.01357456
+
+#Estimate the 75th quantile, which we know is:
+qnorm(.75)
+#[1] 0.6744898
+
+#with the sample quantile:
+quantile(y, .75)
+#Run a monte carlo simulation to learn the expected value and 
+#standard error of this random variable.
+# Set parameters
+n <- 100              # Sample size
+B <- 10000            # Number of simulations
+quantile_vals <- numeric(B)  # Storage for quantile estimates
+
+# Run Monte Carlo simulation
+set.seed(123)         # For reproducibility
+for (i in 1:B) {
+  y <- rnorm(n, 0, 1)
+  quantile_vals[i] <- quantile(y, 0.75)
+}
+
+# Analyze results
+expected_value <- mean(quantile_vals)
+standard_error <- sd(quantile_vals)
+
+# Output
+cat("Expected value of 75th quantile:", expected_value, "\n")
+cat("Standard error:", standard_error, "\n")
