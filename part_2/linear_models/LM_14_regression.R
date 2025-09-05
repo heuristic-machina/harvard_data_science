ff85ebc6715d5dd06124d7bdfdfccd75cf72520c
heuristic_machina <machine_learner@yahoo.com>
2025-09-05 16:13:05 -0700

    rho correlation of the (x,y) value pairs of (father,son) heights

--- part_2/linear_models/LM_14_regression.R
+++ part_2/linear_models/LM_14_regression.R
@@ -37,3 +37,25 @@ galton_heights |> mutate(father_strata = factor(round(father))) |>
   ggplot(aes(father_strata, son)) + 
   geom_boxplot() + 
   geom_point()
+
+
+#14.4 Regression Line
+mu_x <- mean(galton_heights$father)
+mu_y <- mean(galton_heights$son)
+s_x <- sd(galton_heights$father)
+s_y <- sd(galton_heights$son)
+r <- cor(galton_heights$father, galton_heights$son)
+
+galton_heights |> 
+  ggplot(aes(father, son)) + 
+  geom_point(alpha = 0.5) +
+  geom_abline(slope = r * s_y/s_x, intercept = mu_y - r * s_y/s_x * mu_x) 
+
+#the regression formula implies when standardizing the variables:
+#subtracting the average and dividing the standard deviation, then
+#regression line has intercept 0 and a slope equal to the correlation rho
+#the plot above can then be input as the following:
+galton_heights |> 
+  ggplot(aes(scale(father), scale(son))) + 
+  geom_point(alpha = 0.5) +
+  geom_abline(intercept = 0, slope = r) 
