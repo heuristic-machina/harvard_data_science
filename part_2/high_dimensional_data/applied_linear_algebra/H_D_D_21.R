9713376b969d06bba6d58ec8567dd51a479a04bc
heuristic_machina <machine_learner@yahoo.com>
2025-09-10 01:20:38 -0700

    solving linear equations using matrix() and solve()

--- part_2/high_dimensional_data/applied_linear_algebra/H_D_D_21.R
+++ part_2/high_dimensional_data/applied_linear_algebra/H_D_D_21.R
@@ -23,3 +23,20 @@ for(i in 1:m){
 
 all.equal(C, A %*% B)
 #[1] TRUE
+
+#2. Solve the following system of equations using R:
+#x + y + z + w = 10
+#2x + 3y - z - w =5
+#3x - y + 4z - 2w = 15
+#2x + 2y - 2z - 2w = 20
+
+D<-matrix(c(1, 1, 1, 1, 2, 3,
+            -1, -1, 3, -1, 4,
+            -2, 2, 2, -2, -2), 4, 4, byrow=TRUE)
+e<-matrix(c(10, 5, 15, 20))
+solve(D, e)
+#      [,1]
+#[1,]  25.0
+#[2,] -15.0
+#[3,] -12.5
+#[4,]  12.5
