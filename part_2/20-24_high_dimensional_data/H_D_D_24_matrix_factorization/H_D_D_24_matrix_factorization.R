23bc3aab286ae1ee20c7e1127e2bfd30a6a997b9
heuristic_machina <machine_learner@yahoo.com>
2025-09-12 05:44:03 -0700

    heatmap and cross-domain correlation in visualizing principal components of student scores simulation

---
+++ part_2/20-24_high_dimensional_data/H_D_D_24_matrix_factorization/H_D_D_24_matrix_factorization.R
@@ -0,0 +1,55 @@
+#High Dimensional Data 24
+#Matrix Factorization
+
+#Exercises 24.5
+
+#SVD to estimate factors
+#Dataset of grade scores for 100 students in 24 subjects
+#Percentage points received: 0=C, 25=A+, -25=F
+
+#Analysis:Are all students just about as good? Does being 
+#good in one subject imply one will be good in another? How 
+#does the SVD help with all this?
+
+set.seed(1987)
+n <- 100
+k <- 8
+Sigma <- 64  * matrix(c(1, .75, .5, .75, 1, .5, .5, .5, 1),
+                      3, 3) 
+m <- MASS::mvrnorm(n, rep(0, 3), Sigma)
+m <- m[order(rowMeans(m), decreasing = TRUE),]
+y <- m %x% matrix(rep(1, k), nrow = 1) +
+  matrix(rnorm(matrix(n * k * 3)), n, k * 3)
+colnames(y) <- c(paste(rep("Math",k), 1:k, sep="_"),
+                 paste(rep("Science",k), 1:k, sep="_"),
+                 paste(rep("Arts",k), 1:k, sep="_"))
+
+my_image <- function(x, zlim = range(x), ...){
+  colors = rev(RColorBrewer::brewer.pal(9, "RdBu"))
+  cols <- 1:ncol(x)
+  rows <- 1:nrow(x)
+  image(cols, rows, t(x[rev(rows),,drop=FALSE]),
+        xaxt = "n", yaxt = "n",
+        xlab="", ylab="",  col = colors, zlim = zlim, ...)
+  abline(h=rows + 0.5, v = cols + 0.5)
+  axis(side = 1, cols, colnames(x), las = 2)
+}
+
+my_image(y)
+
+#1. How would you describe the data based on this figure?
+
+#The students that test well are at the top of the image and
+# there seems to be three groupings by subject.
+
+
+#2 Examine the correlation between the test scores directly:
+my_image(cor(y), zlim = c(-1,1))
+range(cor(y))
+#[1] 0.4855371 1.0000000
+axis(side = 2, 1:ncol(y), rev(colnames(y)), las = 2)
+
+#Which of the following best describes what you see?
+
+#There is a correlation among all tests, but higher if the tests
+# are in science and math and even higher within each subject.
