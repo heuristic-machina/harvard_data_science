6a3cea38f08ed58da7e82eb080aaca4348766377
heuristic_machina <machine_learner@yahoo.com>
2025-09-14 06:45:07 -0700

    using cut() and quantile minimizing variability and assuring groups have same number of points

--- part_2/25-32_machine_learning/M_L_27_conditional_prob_and_expect/M_L_27_conditional_probabilities_and_expectations.R
+++ part_2/25-32_machine_learning/M_L_27_conditional_prob_and_expect/M_L_27_conditional_probabilities_and_expectations.R
@@ -12,3 +12,20 @@ heights %>%
   group_by(height) %>%  
   summarize(p = mean(sex == "Male")) %>%
   qplot(height, p, data =.)
+
+#2. In the plot we just made, we see high variability for low 
+#values of height. This is because we have few data points in 
+#these strata. This time use the quantile function for quantiles 
+#0.1, 0.2, ..., 0.9 and the cut function to assure each group 
+#has the same number of points. Hint: For any numeric vector x, 
+#you can create groups based on quantiles as we demonstrate below.
+cut(x, quantile(x, seq(0, 1, 0.1)), include.lowest = TRUE)
+
+ps<-seq(0, 1, 0.1)
+heights %>%
+  mutate(g=cut(height,
+               quantile(height, ps),
+               include.lowest=TRUE)) %>%
+  group_by(g) %>%
+  summarize(p=mean(sex=='Male'), height=mean(height)) %>%
+  qplot(height, p, data=.)
