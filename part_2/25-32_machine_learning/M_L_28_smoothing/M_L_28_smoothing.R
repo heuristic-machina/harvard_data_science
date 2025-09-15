9b0c46448b1100e2d5003c9c28254a0025ee215d
heuristic_machina <machine_learner@yahoo.com>
2025-09-15 05:07:03 -0700

    smoothing death counts by date using loess 60 day span

--- part_2/25-32_machine_learning/M_L_28_smoothing/M_L_28_smoothing.R
+++ part_2/25-32_machine_learning/M_L_28_smoothing/M_L_28_smoothing.R
@@ -8,7 +8,8 @@ library(dslabs)
 mnist_27$train |> ggplot(aes(x_1, x_2, color = y)) + geom_point()
 
 #28.5 Local weighted regression
-#weighted range of 3 weeks smoothes line
+#weighted range of 3 weeks smooths line
+#default degrees=2 is for parabolas resulting in more noise
 
 total_days <- diff(range(polls_2008$day))
 span <- 21/total_days
@@ -17,3 +18,26 @@ polls_2008 |> mutate(smooth = fit$fitted) |>
   ggplot(aes(day, margin)) +
   geom_point(size = 3, alpha = .5, color = "grey") +
   geom_line(aes(day, smooth), color = "red")
+
+#Exercises 28.7
+#1. The dslabs package provides the following dataset with 
+#mortality counts for Puerto Rico for 2015-2018.
+
+#Remove data from before May 2018, then use the loess function
+# to obtain a smooth estimate of the expected number of deaths 
+#as a function of date. Plot this resulting smooth function. 
+#Make the span about two months long.
+library(dslabs)
+head(pr_death_counts)
+
+dat<-pr_death_counts %>% filter(date <= "2018-05-01")
+  
+span <- 60 / as.numeric(diff(range(dat$date)))
+fit <- dat %>% mutate(x = as.numeric(date)) %>%
+  loess(deaths ~ x, data = ., span = span, degree = 1)
+
+dat %>%
+  mutate(smooth = predict(fit, as.numeric(date))) %>%
+  ggplot() +
+  geom_point(aes(date, deaths)) +
+  geom_line(aes(date, smooth), lwd = 2, col = 2)
